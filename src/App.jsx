6cee390ce1ee0b24d6a9e47773483bcff4691d85
thieny13072008-afk <thieny13072008@gmail.com>
2026-06-26 15:40:29 +0700

    Submit excercie 1-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import ArticlesSummary from './Articles.jsx'
 import { ARTICLES } from "./data";
-
+import Display from "./Home.jsx";
 
 function App() {
   return(
-    < ArticlesSummary id={ARTICLES[0].id} header={ARTICLES[0].header} date={ARTICLES[0].date} content={ARTICLES[0].content}/>
+    <Display />
   )
 }
 
